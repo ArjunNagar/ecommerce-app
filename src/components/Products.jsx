7c00ed8ba65b409a27df47c2d443e01b8cd85d26
ArjunNagar <arjunnagar579@gmail.com>
2022-09-02 10:38:02 +0530

    inital commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -25,7 +25,7 @@ function Products() {
         <div className="card" key={product.id}>
           <img src={product.image} alt="" />
           <h4>{product.title}</h4>
-          <h5>{product.price}</h5>
+          <h5>${product.price}</h5>
           <button className="btn" onClick={() => handleAdd(product)}>
             Add to cart
           </button>
